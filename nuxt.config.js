29724e120bf3945fe570945b115e625d949f8091
mosesmadale <66674160+mosesmadale@users.noreply.github.com>
2022-07-26 13:08:08 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/my-app/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
